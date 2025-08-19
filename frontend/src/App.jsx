156d28358dafefa28d19d3fdbebfc02cd2af77e2
Iamnishankjain <jnishank123@gmail.com>
2025-08-19 19:46:44 +0530

    use query from tenstack for get request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+
 import HomePage from './pages/HomePage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
+import NotificationPage from './pages/NotificationPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+
+import { useQuery } from '@tanstack/react-query'
+
 
 const App = () => {
+  const {data,isLoading,error} = useQuery({queryKey: ["todos"],
+    
+    queryFn: async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const data = await response.json()
+      return data;
+    }
+  })
+  console.log(data, isLoading, error);
+
   return (
     <div className="h-screen" data-theme="night">
       <Routes>
@@ -15,6 +36,8 @@ const App = () => {
         <Route path="call" element={<CallPage />} />
         <Route path="chat" element={<ChatPage />} />
       </Routes>
+
+      <Toaster/>
     </div>
   )
 }
