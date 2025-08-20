27f2e71c7d7a3a1d6b99bd0d27fbed1a5b8dcfc4
Iamnishankjain <jnishank123@gmail.com>
2025-08-20 19:30:00 +0530

    get profile picture at onboarding page

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import useAuthUser from '../hooks/useAuthUser';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { completeOnboarding } from '../lib/api';
+import { CameraIcon } from 'lucide-react';
 
 const OnboardingPage = () => {
   const {authUser} = useAuthUser();
@@ -23,9 +24,38 @@ const OnboardingPage = () => {
     },
   })
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the mutation to complete onboarding
+    onboardingMutation(formState);
+  }
+
   return (
-    <div>
-      On boarding Page
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
+      <div className='card bg-base-200 w-full max-w-3xl shadow-xl '>
+        <div className='card-body p-6 sm:p-8'>
+          <h1 className='text-2xl sm:test-3xl font-bold text-center mb-6'>Complete Your Profile</h1>
+          <form onSubmit={handleSubmit} className='space-y-6'>
+            {/* profile pic */}
+            <div className='flex flex-col items-center justify-center space-y-4'>
+              {/* Image Preview*/}
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePicture ? (
+                  <img src={formState.profilePicture} alt="profile preview" className='w-full h-full object-cover' />
+                ) : (
+                  <div className='flex items-center justify-center h-full'>
+                    <CameraIcon className='size-12 text-base-content opacity-40'/>
+                  </div>
+                )}
+
+
+              </div>
+            </div>
+
+          </form>
+        </div>
+
+      </div>
     </div>
   )
 }
