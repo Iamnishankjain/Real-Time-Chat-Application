cf5057c7c2ce1d5214d7d38c491077487c8f9021
Iamnishankjain <jnishank123@gmail.com>
2025-08-21 18:37:23 +0530

    side bar complete

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -37,6 +37,24 @@ const Sidebar = () => {
       </nav>
 
       {/*User Profile */}
+      <div className='p-4 border-t border-base-300 mt-auto'>
+        <div className='flex items-center gap-3'>
+          <div className='avatar'>
+            <div className='w-10 rounded-full'>
+              <img src={authUser?.profilePicture} alt="User Avatar" />
+            </div>
+          </div>
+          <div className='flex-1'>
+            <p className='font-semibold text-sm'>
+              {authUser?.fullName}
+            </p>
+            <p className='text-xs text-success flex items-center gap-1'>
+              <span className='size-2 rounded-full bg-success inline-block'/>
+              Online
+            </p>
+          </div>
+        </div>
+      </div>
     </aside>
   )
 }
