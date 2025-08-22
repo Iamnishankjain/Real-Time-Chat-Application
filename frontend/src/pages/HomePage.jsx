b60c425b3c82914dca246a9e9fd0d9e687405e80
Iamnishankjain <jnishank123@gmail.com>
2025-08-22 19:32:24 +0530

    create fetch api for get frinds and users

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getRecommandedUsers, getUserFriends } from '../lib/api.js';
 
 const HomePage = () => {
+
+  const queryClient = useQueryClient();
+  const [outgoingRequest,setOutgoingRequest] = useState([]);
+  const {data:friends=[],isLoading:loadingFriends} = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends
+  })
+
+  const {data:recommandedUsers=[],isLoading:loadingUsers} = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommandedUsers
+  })
+
   return (
     <div>
       
