03606dfda834a65998b265b804c06364354433a0
Iamnishankjain <jnishank123@gmail.com>
2025-08-22 19:13:15 +0530

    theme-selection complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,15 +15,18 @@ import Layout from "./components/Layout.jsx";
 
 // Custom hook to fetch authenticated user data
 import useAuthUser from "./hooks/useAuthUser.js";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
+  const {theme} = useThemeStore();
+
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
 
   if (isLoading) return <PageLoader />;
   return (
-    <div className="h-screen" data-theme="forest">
+    <div className="h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
