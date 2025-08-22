74e8293ded714ebe3eb55f3d8bbd4733d0449f78
Iamnishankjain <jnishank123@gmail.com>
2025-08-23 01:22:16 +0530

    UI of home page complete

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,51 +1,177 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getOutgoingFriendReqs, getRecommandedUsers, getUserFriends, sendFriendRequest } from '../lib/api.js';
+import {
+  getOutgoingFriendReqs,
+  getRecommandedUsers,
+  getUserFriends,
+  sendFriendRequest,
+} from "../lib/api.js";
+import { CheckCircleIcon, MapPinIcon, UserPlusIcon, UsersIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+import FriendCard, { getLanguageFlag } from "../components/FriendCard.jsx";
+import NoFriend from "../components/NoFriend.jsx";
+import NoRecommandedUsersFound from "../components/NoRecommandedUsersFound.jsx";
 
 const HomePage = () => {
-
   const queryClient = useQueryClient();
 
-  const [outgoingRequest,setOutgoingRequest] = useState([
-    new Set()
-  ]);
+  const [outgoingRequest, setOutgoingRequest] = useState([new Set()]);
 
-  const {data:friends=[],isLoading:loadingFriends} = useQuery({
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
     queryKey: ["friends"],
-    queryFn: getUserFriends
-  })
+    queryFn: getUserFriends,
+  });
 
-  const {data:recommandedUsers=[],isLoading:loadingUsers} = useQuery({
+  const { data: recommandedUsers = [], isLoading: loadingUsers } = useQuery({
     queryKey: ["users"],
-    queryFn: getRecommandedUsers
-  })
+    queryFn: getRecommandedUsers,
+  });
 
-  const {data: outgoingFriendReqs} = useQuery({
+  const { data: outgoingFriendReqs } = useQuery({
     queryKey: ["users"],
-    queryFn: getOutgoingFriendReqs
-  })
+    queryFn: getOutgoingFriendReqs,
+  });
 
-  const {mutate: sendRequestMutation,isPending} = useMutation({
+  const { mutate: sendFriendRequestMutation, isPending } = useMutation({
     mutationFn: sendFriendRequest,
-    onSuccess: () => queryClient.invalidateQueries({queryKey: ["outgoingFriendReqs"]})
-  })
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     const outgoingIds = new Set();
-    if(outgoingFriendReqs && outgoingFriendReqs.length>0){
-      outgoingFriendReqs.forEach((req)=>{
+    if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
+      outgoingFriendReqs.forEach((req) => {
         outgoingIds.add(req.id);
-      })
+      });
       setOutgoingRequest(outgoingIds);
     }
-  },[outgoingFriendReqs])
+  }, [outgoingFriendReqs]);
 
   return (
-    <div>
-      
-      Home Page
-    </div>
-  )
-}
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto space-y-10">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+            Your Friends
+          </h2>
+          <Link to="/notifications" className="btn btn-outline btn-sm">
+            <UsersIcon className="mr-2 size-4" /> Friend Requests
+          </Link>
+        </div>
 
-export default HomePage
+        {loadingFriends ? (
+          <div className="flex justify-center py-12">
+            <span className="loading loading-spinner loading-lg" />
+          </div>
+        ) : friends.length === 0 ? (
+          <NoFriend />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {friends.map((friend) => {
+              <FriendCard key={friend._id} friend={friend} />;
+            })}
+          </div>
+        )}
+
+        {/*Recommanded friend */}
+
+        <section>
+          <div className="mb-6 sm:mb-8">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+              <div>
+                <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+                  Connect with Users
+                </h2>
+                <p className="opacity-70">
+                  Discover perfect language exchange partners based on your
+                  profile
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {loadingUsers ? (
+            <div className="flex justify-center py-12">
+              <span className="loading loading-spinner loading-lg" />
+            </div>
+          ) : recommandedUsers.length === 0 ? (
+            <NoRecommandedUsersFound />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {recommandedUsers.map((user) => {
+                const hasRequestBeenSent = outgoingFriendReqs.has(user._id);
+                return (
+                  <div
+                    key={user._id}
+                    className="card bg-base-200 hover:shadow-lg transition-all duration-300"
+                  >
+                    <div className="card-body p-5 space-y-4">
+                      <div className="flex items-center gap-3">
+                        <div className="avatar size-16 rounded-full">
+                          <img src={user.profilePicture} alt={user.fullName} />
+                        </div>
+
+                        <div>
+                          <h3 className="font-semibold text-lg">
+                            {user.fullName}
+                          </h3>
+                          {user.location && (
+                            <div className="flex items-center text-xs opacity-70 mt-1">
+                              <MapPinIcon className="size-3 mr-1" />
+                              {user.location}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+
+                      {/*language */}
+                      <div className="flex flex-wrap gap-1.5 mb-3">
+                        <span className="badge badge-secondary text-xs">
+                          {getLanguageFlag(user.nativeLanguage)}
+                          Native: {capitialize(user.nativeLanguage)}
+                        </span>
+                        <span className="badge badge-secondary text-xs">
+                          {getLanguageFlag(user.learningLanguage)}
+                          Learning: {capitialize(user.learningLanguage)}
+                        </span>
+                      </div>
+
+                      {user.bio && <p className="text-sm opacity-70">{user.bio}</p>}
+
+                      {/*action button */}
+
+                      <button className={`btn w-full mt-2 ${
+                        hasRequestBeenSent ? "btn-disabled" : "btn-primary"
+                      }`}
+                      onClick={()=> sendFriendRequestMutation(user._id)}
+                      disabled={hasRequestBeenSent || isPending}
+                      >
+                        {hasRequestBeenSent ? (
+                          <>
+                            <CheckCircleIcon className="size-4 mr-2"/>
+                            Request Sent
+                          </>
+                        ) : (
+                          <>
+                            <UserPlusIcon className="size-4 mr-2"/>
+                            Send Friend Request
+                          </>
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
+
+
+const capitialize = (str) => str.charAt(0).toUpperCase()+str.slice(1);
