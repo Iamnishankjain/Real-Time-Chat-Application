a5fa439064ec276373f7be67b3581131eca486d0
Iamnishankjain <jnishank123@gmail.com>
2025-08-23 18:13:33 +0530

    acceptFriendRequest api implement in api.js

--- frontend/src/pages/NotificationPage.jsx
+++ frontend/src/pages/NotificationPage.jsx
@@ -1,12 +1,20 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
 import { getFriendRequest } from '../lib/api.js';
 
-const NotificationPage = () => {
-  const queryClient = useQueryClient();
+const NotificationPage = () => {   
+  const queryClient = useQueryClient();        //fetching
   const {data: friendRequests,isLoading} = useQuery({
     queryKey: ["friendRequests"],
     queryFn: getFriendRequest,
+  })
+
+  const {mutate: acceptRequestMutation,isPending} = useMutation({
+    mutationFn: acceptFriendRequest,
+    onSuccess: ()=> {
+      queryClient.invalidateQueries({queryKey: ["friendRequests"]});
+      queryClient.invalidateQueries({queryKey: ["friends"]})
+    }
   })
 
   return (
