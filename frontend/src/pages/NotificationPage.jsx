70049473223a7f01395d420d873e5f8ce36812ab
Iamnishankjain <jnishank123@gmail.com>
2025-08-23 18:05:57 +0530

    make api of getfriendrequest in api.js

--- frontend/src/pages/NotificationPage.jsx
+++ frontend/src/pages/NotificationPage.jsx
@@ -1,9 +1,17 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
+import { getFriendRequest } from '../lib/api.js';
 
 const NotificationPage = () => {
+  const queryClient = useQueryClient();
+  const {data: friendRequests,isLoading} = useQuery({
+    queryKey: ["friendRequests"],
+    queryFn: getFriendRequest,
+  })
+
   return (
     <div>
-      
+      Notifiaction
     </div>
   )
 }
