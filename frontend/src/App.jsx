a7730caa79166751b5835b1de3a9e0cda25b0e9f
Iamnishankjain <jnishank123@gmail.com>
2025-08-24 07:34:24 +0530

    set chat path route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -66,12 +66,18 @@ const App = () => {
           }
         />
         <Route
-          path="call"
+          path="/call"
           element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
         />
         <Route
-          path="chat"
-          element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}
+          path="/chat/:id"
+          element={isAuthenticated && isOnboarded ? (
+            <Layout showSidebar={false}>
+              <ChatPage/>
+            </Layout>
+          ) : (
+            <Navigate to={!isAuthenticated ? "/login":"/onboarding"}/>
+          )}
         />
         <Route
           path="/onboarding"
