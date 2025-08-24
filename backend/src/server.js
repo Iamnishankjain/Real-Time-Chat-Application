0bb487faa19825a80ddd5da09dd159cac2c86d35
Iamnishankjain <jnishank123@gmail.com>
2025-08-24 10:31:28 +0530

    ready to deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 dotenv.config();
+import path from 'path';
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -20,12 +21,21 @@ app.use(cors(
 ));
 const PORT = process.env.PORT;
 
+const __dirname=path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  })
+}
 
 app.listen(PORT, () => { 
   console.log('Server is running on http://localhost:' + PORT);
